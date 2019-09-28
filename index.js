9fd903a48304665237a1996232a729a6b2be568d
Angainor <shaphi@fastwebnet.it>
2019-09-28 11:49:45 +0200

    update RNG engine with xorshift128+

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const http = require("http");
-// Require the module
-const Random = require('rng');
-// Instantiate a new Mersenne Twister with a seed
-var mt = new Random.MT(Date.now());
+const xorshift = require('xorshift');
+
 setInterval(function() {
     http.get("http://telegram-rolldice.herokuapp.com/cool");
-	mt = new Random.MT(Date.now());
 }, 300000); // every 5 minutes (300000)
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -57,7 +54,7 @@ bot.hears(/./, (message) => {
 		var splittedThrow = "";
 		
 		for(var i = 0; i<number;i++){
-			var diceResult = mt.range(min, max);
+			var diceResult = Math.floor(min + xorshift().random() * (max - min));
 			splittedThrow += diceResult + " + ";
 			result += diceResult;
 			if(debug){
